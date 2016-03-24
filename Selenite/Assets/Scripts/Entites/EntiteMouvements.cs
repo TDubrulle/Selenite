8102663c0b48ae5cbf105ab7380f3cb7f90ad9f6
unknown <t.dubrulle@J001-0129-03.etudiant.rubika-edu.com.local>
2016-03-24 15:40:21 +0100

    Ajout projet

---
+++ Selenite/Assets/Scripts/Entites/EntiteMouvements.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**Classe représentant les possibilités mouvementalistes d'une entité.*/
+public class EntiteMouvements {
+
+    /**Gravité vers le bas. Commune à toutes les entités.*/
+    public static float gravite = 0.5f;
+
+    public Vector2 vitesse;
+
+    protected Vector2 vitesseCourante;
+
+    public Transform entiteTransform;
+
+    // Use this for initialization
+    public EntiteMouvements(Transform entite) {
+        vitesseCourante = new Vector2();
+        entiteTransform = entite;
+	}
+	
+    /**Met à jour la position de l'entité.*/
+	public void UpdatePosition() {
+        vitesseCourante = (vitesseCourante + Vector2.down) * 0.25f;
+        
+        //Si on rentre en collision avec un objet, on se place juste à l'extrémité de celui-ci.
+        //TTODO
+        entiteTransform.position += new Vector3(vitesseCourante.x, vitesseCourante.y, entiteTransform.position.z);
+    }
+}
---
+++ Selenite/Assets/Scripts/Entites/Joueur.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Joueur : MonoBehaviour {
+
+    EntiteMouvements mouvements;
+
+	// Use this for initialization
+	void Awake () {
+        mouvements = new EntiteMouvements(this.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mouvements.UpdatePosition();
+	}
+}
